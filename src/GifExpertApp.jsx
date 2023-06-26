8311286d78d1a3d8b2aa8a3d1fe2e9e67fbba8fa
Juan Camilo Puentes <jcpsandoval94@gmail.com>
2023-06-26 09:50:38 -0500

    Se agrega boton de agregar nuevo item a la lista

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -5,6 +5,10 @@ export const GifExpertApp = () => {
 
   const [categories, setCategories] = useState(['one punch', 'dragon ball'])
 
+  const onAddCategory = () => {
+    setCategories(['Hunter x Hunter', ...categories])
+  }
+
   return (
     <>
       {/* titulo */}
@@ -13,6 +17,8 @@ export const GifExpertApp = () => {
       {/* Input */}
 
       {/* Listados de Gif */}
+      <button onClick={onAddCategory}>Agregar</button>
+
       <ol>
         {categories.map((item, index) => (
           <li key={index}>{item}</li>
